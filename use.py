32bf77d07943a1197c094ba4fb2b3576a6fbb256
cute apple <hidden>
2018-07-31 16:40:57 +0800

    clear

--- use.py
+++ use.py
@@ -18,9 +18,10 @@ model = keras.models.load_model('model.h5')
 
 import numpy as np
 #input : numpy array, batch*28*28*1, range: [0,1] 
-canvas = np.zeros((1,28,28,1)) # first 1 is batch size
+canvas = np.zeros((2,28,28,1)) # 2 is batch size (2 images)
 
 #predict (batch)
 y_batch = model.predict(canvas) # batch predict
 
+#can see 2 result
 print(y_batch)
